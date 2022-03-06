d10f13716ed1d1b3ffe6fcc0771481d14625a479
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-03-06 19:38:20 -0300

    MenuLink Component

---
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -0,0 +1,25 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { MenuLink } from '.';
+
+describe('<MenuLink />', () => {
+  it('should render a link', () => {
+    renderTheme(<MenuLink link="http://www.google.com/">Children</MenuLink>);
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
+      'target',
+      '_self',
+    );
+  });
+
+  it('should render open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http://www.google.com/" newTab={true}>
+        Children
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'Children' })).toHaveAttribute(
+      'target',
+      '_blank',
+    );
+  });
+});
