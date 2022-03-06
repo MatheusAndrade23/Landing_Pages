c0b628c5a6802d5b3345279c37912e1ede00b63c
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-03-06 20:54:49 -0300

    NavLink Component

---
+++ src/components/NavLinks/mock.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    children: 'Link 1',
+    link: '#target1',
+    newTab: false,
+  },
+  {
+    children: 'Link 2',
+    link: '#target1',
+    newTab: false,
+  },
+  {
+    children: 'Link 3',
+    link: '#target1',
+    newTab: false,
+  },
+  {
+    children: 'Link 4',
+    link: '#target1',
+    newTab: false,
+  },
+  {
+    children: 'Link 5',
+    link: '#target1',
+    newTab: false,
+  },
+  {
+    children: 'Link 6',
+    link: '#target1',
+    newTab: false,
+  },
+  {
+    children: 'Link 7',
+    link: '#target1',
+    newTab: false,
+  },
+  {
+    children: 'Link 8',
+    link: '#target1',
+    newTab: false,
+  },
+  {
+    children: 'Link 9',
+    link: '#target1',
+    newTab: false,
+  },
+  {
+    children: 'Link 10',
+    link: '#target1',
+    newTab: false,
+  },
+];
