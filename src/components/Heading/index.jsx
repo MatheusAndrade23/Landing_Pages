3a8a2f765cf240e9a15314fa6920d70c89bc340a
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-03-06 18:12:36 -0300

    Heading Component

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,9 +1,20 @@
 import P from 'prop-types';
 import * as Styled from './styles';
 
-export const Heading = ({ children, colorDark = true, as = 'h1', size = 'huge', uppercase = false }) => {
+export const Heading = ({
+  children,
+  colorDark = true,
+  as = 'h1',
+  size = 'huge',
+  uppercase = false,
+}) => {
   return (
-    <Styled.Title colorDark={colorDark} as={as} size={size} uppercase={uppercase}>
+    <Styled.Title
+      colorDark={colorDark}
+      as={as}
+      size={size}
+      uppercase={uppercase}
+    >
       {children}
     </Styled.Title>
   );
