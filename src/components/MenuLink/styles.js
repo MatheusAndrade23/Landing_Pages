5f1ea72350e00454951dfc014fdd40471f5e05e3
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-03-06 20:30:09 -0300

    MenuLink Component

--- src/components/MenuLink/styles.js
+++ src/components/MenuLink/styles.js
@@ -12,7 +12,7 @@ export const Container = styled.a`
     &::after {
       content: '';
       position: absolute;
-      bottom: 0;
+      bottom: 0.7rem;
       left: 50%;
       width: 0;
       height: 0.2rem;
