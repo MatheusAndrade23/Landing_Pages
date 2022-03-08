6ebc0118e91a4be74b00bb6a1d46bfca0a33c545
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-03-08 19:47:29 -0300

    Background Component

---
+++ src/components/SectionBackground/index.jsx
@@ -0,0 +1,16 @@
+import P from 'prop-types';
+import * as Styled from './styles';
+import { SectionContainer } from '../SectionContainer';
+
+export const SectionBackground = ({ children, background = false }) => {
+  return (
+    <Styled.Container background={background}>
+      <SectionContainer>{children}</SectionContainer>
+    </Styled.Container>
+  );
+};
+
+SectionBackground.propTypes = {
+  children: P.node.isRequired,
+  background: P.bool,
+};
---
+++ src/components/SectionBackground/stories.jsx
@@ -0,0 +1,31 @@
+import { SectionBackground } from '.';
+import { SectionContainer } from '../SectionContainer';
+
+export default {
+  title: 'SectionBackground',
+  component: SectionBackground,
+  args: {
+    children: (
+      <div>
+        <h1>SectionBackground</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque error
+          sint saepe commodi cumque vero. Perferendis, quidem vitae. Suscipit
+          harum necessitatibus totam quaerat numquam culpa obcaecati aliquid
+          molestiae ad libero.
+        </p>
+      </div>
+    ),
+  },
+  argTypes: {
+    children: { type: '' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <SectionBackground {...args} />
+    </div>
+  );
+};
