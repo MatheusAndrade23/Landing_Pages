3416f7b39fae704026cbc7564b2f02a4c7048aba
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-03-10 20:11:10 -0300

    NavLinks Component

--- src/components/NavLinks/mock.js
+++ src/components/NavLinks/mock.js
@@ -6,47 +6,47 @@ export default [
   },
   {
     children: 'Link 2',
-    link: '#target1',
+    link: '#target2',
     newTab: false,
   },
   {
     children: 'Link 3',
-    link: '#target1',
+    link: '#target3',
     newTab: false,
   },
   {
     children: 'Link 4',
-    link: '#target1',
+    link: '#target4',
     newTab: false,
   },
   {
     children: 'Link 5',
-    link: '#target1',
+    link: '#target5',
     newTab: false,
   },
   {
     children: 'Link 6',
-    link: '#target1',
+    link: '#target6',
     newTab: false,
   },
   {
     children: 'Link 7',
-    link: '#target1',
+    link: '#target7',
     newTab: false,
   },
   {
     children: 'Link 8',
-    link: '#target1',
+    link: '#target8',
     newTab: false,
   },
   {
     children: 'Link 9',
-    link: '#target1',
+    link: '#target9',
     newTab: false,
   },
   {
     children: 'Link 10',
-    link: '#target1',
+    link: '#target10',
     newTab: false,
   },
 ];
