96754c009d3030dd7dc1aa5a910b3df3d9a29259
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-03-13 17:25:58 -0300

    FooterComponent

---
+++ src/components/Footer/Footer.test.jsx
@@ -0,0 +1,11 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { Footer } from '.';
+
+describe('<Footer />', () => {
+  it('should render', () => {
+    const { container } = renderTheme(<Footer html={'<h1>Olá</h1>'} />);
+    expect(screen.getByRole('heading', { name: 'Olá' })).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ src/components/Footer/stories.jsx
@@ -0,0 +1,20 @@
+import { Footer } from '.';
+
+export default {
+  title: 'Footer',
+  component: Footer,
+  args: {
+    html: `<p>Feito com por Otávio Miranda e melhorado por Matheus Andrade</p>`,
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <Footer {...args} />
+    </div>
+  );
+};
