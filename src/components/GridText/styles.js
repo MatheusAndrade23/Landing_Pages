a4879efedf4a963bbef1d900958861b405f11909
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-03-13 20:23:49 -0300

    Update styles.js

--- src/components/GridText/styles.js
+++ src/components/GridText/styles.js
@@ -16,6 +16,12 @@ export const Grid = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
     gap: ${theme.spacings.large};
+    width: 100%;
+    overflow: hidden;
+
+    @media ${theme.media.lteMedium} {
+      grid-template-columns: 1fr;
+    }
   `}
 `;
 
