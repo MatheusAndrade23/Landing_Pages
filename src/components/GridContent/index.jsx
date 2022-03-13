56aa8a31c4f8f3a761d60045c323ca590fbcb348
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-03-13 11:24:34 -0300

    GridComponent Component

---
+++ src/components/GridContent/index.jsx
@@ -0,0 +1,26 @@
+import P from 'prop-types';
+import * as Styled from './styles';
+import { Heading } from '../Heading';
+import { SectionBackground } from '../SectionBackground';
+import { TextComponent } from '../TextComponent';
+
+export const GridContent = ({ title, html, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading uppercase colorDark={!background}>
+          {title}
+        </Heading>
+        <Styled.Html>
+          <TextComponent>{html}</TextComponent>
+        </Styled.Html>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridContent.propTypes = {
+  title: P.string.isRequired,
+  html: P.string.isRequired,
+  background: P.bool,
+};
