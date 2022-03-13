cbe350493967b01e52845bdfa4680416748d639d
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-03-12 23:14:46 -0300

    Menu Component

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -2,8 +2,13 @@ import styled, { css } from 'styled-components';
 import { Container as SectionContainer } from '../SectionContainer/styles';
 import { Title as Heading } from '../Heading/styles';
 
+const menuVisible = (theme) => css`
+  visibility: visible;
+  opacity: 1;
+`;
+
 export const Container = styled.div`
-  ${({ theme }) => css`
+  ${({ theme, visible }) => css`
     position: fixed;
     z-index: 5;
     top: 0;
@@ -12,6 +17,7 @@ export const Container = styled.div`
     width: 100%;
     border-bottom: ${theme.colors.MediumGray};
     background: ${theme.colors.white};
+    transition: all 300ms ease-in-out;
 
     > ${SectionContainer} {
       padding-top: 0;
@@ -25,6 +31,9 @@ export const Container = styled.div`
 
     @media ${theme.media.lteMedium} {
       height: 100vh;
+      visibility: hidden;
+      opacity: 0;
+      ${visible && menuVisible(theme)}
 
       > ${SectionContainer} {
         display: grid;
@@ -59,5 +68,28 @@ export const MenuContainer = styled.div`
 `;
 
 export const Button = styled.button`
-  ${({ theme }) => css``}
+  ${({ theme, visible }) => css`
+    z-index: 6;
+    position: fixed;
+    top: 2rem;
+    right: 2rem;
+    width: 4rem;
+    height: 4rem;
+    background: ${theme.colors.primaryColor};
+    color: ${theme.colors.white};
+    border: none;
+    display: none;
+    pointer-events: ${visible ? 'none' : 'all'};
+
+    @media ${theme.media.lteMedium} {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    > svg {
+    width: 2.5rem;
+    height: 2.5rem;
+
+  `}
 `;
