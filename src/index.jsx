9fc617a222b10ea33d803a131af4fbdba60d6ba4
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-03-26 12:02:02 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+} from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
-import Home from './templates/App';
+import Home from './templates/Home';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="*" element={<Home />} />
+        </Switch>
+      </Router>
       <GlobalStyles />
     </ThemeProvider>
   </React.StrictMode>,
