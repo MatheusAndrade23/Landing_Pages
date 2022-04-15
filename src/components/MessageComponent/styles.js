0e34e8ef298d551b31dd9fd6f7d31d8ba4ba6e28
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-04-15 11:36:20 -0300

    Message Component

---
+++ src/components/MessageComponent/styles.js
@@ -0,0 +1,24 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    position: absolute;
+    background-color: ${theme.colors.white};
+    padding: 0 40px;
+    width: max-content;
+    z-index: 10;
+    border: 2px solid ${theme.colors.secondaryColor};
+    border-radius: 10px;
+
+    top: 70px;
+    right: 20px;
+  `}
+`;
+
+export const Text = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.primaryColor};
+    font-style: bold;
+    font-family: ${theme.fonts.family.secondary};
+  `}
+`;
